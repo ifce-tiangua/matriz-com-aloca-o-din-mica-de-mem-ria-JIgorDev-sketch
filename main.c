35eb856feccd7304c3832f18ecb487f543248160
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-08 10:22:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-	int *linha, *coluna, aux1, aux2, aux3;
+	int *matriz, linha, coluna, aux1, aux2;
 
 	scanf("%d %d", &linha, &coluna);
+    
+    matriz = (int**)malloc(linha * sizeof(int*));
 	
-	aux3 = aux1 * aux2;
-	
-	int dados_da_matriz[aux3];
-	
-	scanf("%d", &dados_da_matriz[aux3]);
-	
-	
-	linha = (int *)malloc(aux1 * sizeof(int)); 	
-	coluna = (int *)malloc(aux2 * sizeof(int)); 
-	
-	if(linha == NULL && coluna == NULL){
+	if(matriz == NULL){
 	   printf("[Matriz vazia]\n");
 	   return 1;
 		
@@ -24,14 +16,11 @@ int main(){
 	
 	for(aux1 = 0; aux1 <= linha; aux1++){
 		for(aux2 = 0; aux2 <= coluna; aux2++){
-			printf("%d", linha[aux1]);
-			printf("%d", coluna[aux2]);	
+			printf("%d", matriz[aux1][aux2]);	
 		}
 	}
 	
-	free(linha);
-	free(coluna);
-	
+	free(matriz);
 	
 	
 	
