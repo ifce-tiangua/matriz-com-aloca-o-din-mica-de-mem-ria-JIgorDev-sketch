0d09141d0f38b45ee6f09af9a858236f4c3bc398
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-08 10:28:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+
+	
+	
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,21 +11,31 @@ int main(){
     
     matriz = (int**)malloc(linha * sizeof(int*));
 	
-	if(matriz == NULL){
+	if(matriz == NULL){ //caso a matriz seja vazia
 	   printf("[Matriz vazia]\n");
 	   return 1;
 		
 	}
 	
-	for(aux1 = 0; aux1 <= linha; aux1++){
-		for(aux2 = 0; aux2 <= coluna; aux2++){
+	for(aux1 = 0; aux1 < linha; aux1++){ 
+	matriz[aux1]= (int *) malloc(coluna * sizeof(int));
+	if(matriz[aux1] == NULL)
+	return 1;
+	}
+	
+	
+	
+	for(aux1 = 0; aux1 < linha; aux1++){
+		for(aux2 = 0; aux2 < coluna; aux2++){
 			printf("%d", matriz[aux1][aux2]);	
 		}
 	}
 	
+	for(aux1 = 0; aux1 < linha; aux1++){
+		free(matriz[aux1]);
+	}
+	
 	free(matriz);
-	
-	
 	
 return 0;
 }
