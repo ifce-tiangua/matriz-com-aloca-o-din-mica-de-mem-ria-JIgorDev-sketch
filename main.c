25e5d355dee06f423cc99dec4bf277f7bde01df2
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-19 10:09:01 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 int main(){
     int **matriz, linha, coluna, i, j; 
     scanf("%d %d", &linha, &coluna);
-
+    
     if(linha == 0 || coluna == 0){
         printf("[Matriz vazia]\n");
         return 1;
     }
-
+    
     matriz = (int **)malloc(linha * sizeof(int *));
     
     for(i = 0; i < linha; i++){ 
-        matriz[i] = (int *)malloc(coluna * sizeof(int));
+        matriz[i]= (int *)malloc(coluna * sizeof(int));
         if(matriz[i] == NULL){
             printf("[Matriz vazia]\n");
             return 1;
@@ -28,19 +28,18 @@ int main(){
     
     for(i = 0; i < linha; i++){
         for(j = 0; j < coluna; j++){
-            printf("%d", matriz[i][j]);
-            if(j < coluna - 1) {
+            printf("%d", matriz[i][j]);    
+            if(j < coluna - 1)
                 printf(" ");
-            }
         }
         printf("\n");
     }
     
     for(i = 0; i < linha; i++){ 
-        free(matriz[i]);
+        free(matriz[i]); 
     }
     
-    free(matriz);
-
+    free(matriz); 
+    
     return 0;
 }
