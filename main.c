345eb96a93eaaecbb8e015f32b5e46e81b02bcfc
JIgorDev-sketch <joaoigorsf990@gmail.com>
2024-11-19 16:29:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int **matriz, linhas, colunas, i, j;
-
-    scanf("%d %d", &linhas, &colunas);
-
-    if (linhas == 0 || colunas == 0) {
-        printf("[matriz vazia]\n");
+int main(){
+    int **matriz, linha, coluna, i, j; 
+    scanf("%d %d", &linha, &coluna);
+    
+    if(linha == 0 || coluna == 0){
+        printf("[Matriz vazia]\n");
         return 0;
     }
-
-    matriz = (int **)malloc(linhas * sizeof(int *));
-    if (matriz == NULL) {
-        printf("[matriz vazia]\n");
+    
+    matriz = (int **)malloc(linha * sizeof(int *));
+    
+    if(matriz == NULL){
+        printf("[Matriz vazia]\n");
         return 0;
     }
-
-    for (i = 0; i < linhas; i++) {
-        matriz[i] = (int *)malloc(colunas * sizeof(int));
-        if (matriz[i] == NULL) {
-            printf("[matriz vazia]\n");
+    
+    for(i = 0; i < linha; i++){ 
+        matriz[i] = (int *)malloc(coluna * sizeof(int));
+        if(matriz[i] == NULL){
+            printf("[Matriz vazia]\n");
             return 0;
         }
     }
-
-    for (i = 0; i < linhas; i++) {
-        for (j = 0; j < colunas; j++) {
+    
+    for(i = 0; i < linha; i++){ 
+        for(j = 0; j < coluna; j++){
             scanf("%d", &matriz[i][j]);
         }
     }
-
-    for (i = 0; i < linhas; i++) {
-        for (j = 0; j < colunas; j++) {
-            printf("%d", matriz[i][j]);
-            if (j < colunas - 1)
+    
+    for(i = 0; i < linha; i++){
+        for(j = 0; j < coluna; j++){
+            printf("%d", matriz[i][j]);    
+            if(j < coluna - 1)
                 printf(" ");
         }
         printf("\n");
     }
-
-    for (i = 0; i < linhas; i++) {
-        free(matriz[i]);
+    
+    for(i = 0; i < linha; i++){ 
+        free(matriz[i]); 
     }
-    free(matriz);
-
+    
+    free(matriz); 
+    
     return 0;
 }
